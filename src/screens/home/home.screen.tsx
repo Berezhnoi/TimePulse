e71b77e991a8c73ee30d7de642aa50d58ce46d87
IgorBerezhnoi <iberezhnoi@qualium-systems.com.ua>
2023-11-04 13:36:02 +0100

    charts

--- src/screens/home/home.screen.tsx
+++ src/screens/home/home.screen.tsx
@@ -2,18 +2,42 @@
 import React from 'react';
 
 // Components
-import {View, Text} from 'react-native';
+import {View} from 'react-native';
+import {BarChart} from 'react-native-gifted-charts';
+
+// Hooks
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
 
 // Types
 import {HomeScreenProps} from './home.types';
 
 // Styles
+import {commonStyles} from 'styles';
 import styles from './home.styles';
 
 const HomeScreen: React.FC<HomeScreenProps> = () => {
+  const insets = useSafeAreaInsets();
+
+  const barData = [
+    {value: 8, label: 'M'},
+    {value: 8, label: 'T'},
+    {value: 8, label: 'W', frontColor: 'lightgray'},
+    {value: 8, label: 'T'},
+    {value: 8, label: 'F'},
+    {value: 8, label: 'S'},
+    {value: 14, label: 'S'},
+  ];
+
   return (
-    <View style={styles.container}>
-      <Text>HomeScreen</Text>
+    <View style={[styles.container, {paddingTop: insets.top}]}>
+      <BarChart
+        barWidth={22}
+        barBorderRadius={4}
+        frontColor="#177AD5"
+        data={barData}
+        // yAxisThickness={0}
+        // xAxisThickness={0}
+      />
     </View>
   );
 };
--- src/screens/home/home.styles.ts
+++ src/screens/home/home.styles.ts
@@ -1,7 +1,12 @@
 import {StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingHorizontal: 10,
+  },
 });
 
 export default styles;
